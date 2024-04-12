dfe8c1107aac3717fbcfc3acd6c49978f24918b6
Ollimpia <Ollimpia@DESKTOP-0MDOKUK>
2024-04-12 14:13:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	const unsigned int D1 = 3;
+	const unsigned int D2 = 5;
+
+	double ary[D1][D2];
+	int count = 15;
+	double vault = 0;
+
+	for (int i = 0; i < D1; i++) 
+	{
+		for (int j = 0; j < D2; j++) 
+		{
+			cin >> ary[i][j];
+		}
+	}
+
+	cout << endl;
+
+	cout << endl;
+
+	for (int i = 0; i < D1; i++)
+	{
+		for (int j = 0; j < D2; j++)
+		{
+			cout << "ary[" << i << "," << j << "]= " << ary[i][j] << "\n";
+		}
+	}
+	cout << endl;
+
+	for (int i = 0; i < D1; i++)
+	{
+		for (int j = 0; j < D2; j++)
+		{
+			vault += ary[i][j];
+		}
+	}
+
+	cout << "Сумма всех чисел массива равна: " << vault << "\n";
+	cout << "Среднее арифметическое равно: " << vault / count << "\n";
+}
